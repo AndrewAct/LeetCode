30d780bb6a144998d34a11312a48a58efd2e00da
Andrew Chen <andrew47ohio@gmail.com>
2022-07-23 00:27:45 -0400

    Rotate Array
    Two pointers

---
+++ Python/189RotateArr.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        self.reverse(nums, 0, len(nums)-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, len(nums)-1)
+        
+    def reverse(self, nums, start, end):
+        while start<end:
+            tmp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = tmp
+            start += 1 
+            end -= 1 
